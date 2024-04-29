b6037d8deb240b36ea71fc2bd0b4e88c0ea7039d
buvanes0000 <129579768+buvanes0000@users.noreply.github.com>
2024-04-29 20:53:35 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -29,7 +29,7 @@ const sections = document.querySelectorAll("section");
 function handleScrollAnimation() {
   sections.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 280;
+    let offset = sec.offsetTop - 520;
     let height = sec.offsetHeight;
     if (top >= offset && top < offset + height) {
       sec.classList.add("show");
@@ -42,7 +42,50 @@ function handleScrollAnimation() {
 // Call the function initially to set the correct animation states
 handleScrollAnimation();
 
-// Call the function whenever the window is scrolled
-window.onscroll = () => {
+// Event listener for window scroll
+window.addEventListener("scroll", () => {
   handleScrollAnimation();
-};
+  
+  // Show or hide scroll-to-top button
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Get the button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+
+// Smooth scroll functionality
+const scrollLinks = document.querySelectorAll('a[href^="#"]');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            const offsetTop = targetElement.offsetTop;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+
+function submitForm(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  // Your form submission logic here (e.g., AJAX request or PHP code)
+}
